85fa7080ab6abf74fe7482850e5f9106c323cf5c
Leo Bernard <admin@leolabs.org>
2019-06-12 11:40:08 +0200

    :sparkles: Add a 16px grid to components

--- .storybook/config.ts
+++ .storybook/config.ts
@@ -1,4 +1,15 @@
-import { configure } from "@storybook/react";
+import { configure, addParameters } from "@storybook/react";
+import { create } from "@storybook/theming";
+
+addParameters({
+  options: {
+    theme: create({
+      base: "light",
+      gridCellSize: 16,
+    }),
+  },
+});
+
 // automatically import all files ending in *.stories.tsx
 const req = require.context("../src", true, /\.stories\.tsx$/);
 
