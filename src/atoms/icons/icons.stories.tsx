c69b553f8ca79f45a381ef88703d9e2e5185b715
Marcus Weiner <marcus.weiner@gmail.com>
2019-06-12 13:32:30 +0200

    📝 Document icons collection

---
+++ src/atoms/icons/icons.stories.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+
+storiesOf("Atoms / Icons", module).add("Inventory", () => (
+  <p>
+    See{" "}
+    <a href="https://feathericons.com/" target="_blank">
+      Feather Icons
+    </a>
+  </p>
+));
