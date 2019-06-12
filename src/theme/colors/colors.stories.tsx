9035a63247aa01f452ff27051f528622f0e5d4e3
Leo Bernard <admin@leolabs.org>
2019-06-12 11:41:54 +0200

    :lipstick: Improve styling of colors

--- src/theme/colors/colors.stories.tsx
+++ src/theme/colors/colors.stories.tsx
@@ -10,9 +10,15 @@ const Colors = styled.div`
 `;
 
 const Color = styled.div<{ color: string }>`
-  width: 128px;
-  height: 128px;
+  display: flex;
+  align-items: flex-end;
+  width: 64px;
+  height: 64px;
+  padding: 0.5rem;
+  color: #fff;
+  text-shadow: 0 0 3px #000;
   margin-right: 1rem;
+  margin-bottom: 1rem;
   border-radius: 0.5rem;
   background-color: ${p => p.color};
 
@@ -21,14 +27,14 @@ const Color = styled.div<{ color: string }>`
   }
 `;
 
-storiesOf("Theme", module).add("Colors", () => (
+storiesOf("Theme / Colors", module).add("Color Swatches", () => (
   <>
     {Object.keys(colors).map(c => (
       <div>
         <h2>{c}</h2>
         <Colors>
           {Object.keys(colors[c]).map(color => (
-            <Color color={colors[c][color]} />
+            <Color color={colors[c][color]}>{color}</Color>
           ))}
         </Colors>
       </div>
