32b9088800478bc2b85f26fad34dc53790b69d35
Leo Bernard <admin@leolabs.org>
2019-06-12 12:59:36 +0200

    :sparkles: Add story source addon

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -16,6 +16,18 @@ module.exports = ({ config }) => {
       },
     ],
   });
+
+  config.module.rules.push({
+    test: /\.stories\.tsx?$/,
+    loaders: [
+      {
+        loader: require.resolve("@storybook/addon-storysource/loader"),
+        options: { parser: "typescript" },
+      },
+    ],
+    enforce: "pre",
+  });
+
   config.resolve.extensions.push(".ts", ".tsx");
   return config;
 };
