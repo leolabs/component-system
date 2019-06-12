ca94aea2c17181fb4ccd10d1326326b4a71d04d7
Leo Bernard <admin@leolabs.org>
2019-06-12 13:07:03 +0200

    :art: Format code with prettier

--- src/atoms/ribbon/ribbon.stories.tsx
+++ src/atoms/ribbon/ribbon.stories.tsx
@@ -2,24 +2,21 @@ import { storiesOf } from "@storybook/react";
 import React from "react";
 
 import { orange } from "../../theme/colors/colors";
-import { Theme } from "../../theme/theme/theme";
+import { Theme } from "../../theme/theme-context/theme-context";
 
 import { Ribbon } from "./ribbon";
 
-storiesOf("Ribbon", module)
-  .add("base", () => (
-    <Theme color={orange}>
-      <div
-        style={{
-          backgroundColor: 'blue',
-          width: 200,
-          height: 200,
-          position: 'relative',
-        }}
-      >
-        <Ribbon>
-          Ribbon Text
-        </Ribbon>
-      </div>
-    </Theme>
-  ));
+storiesOf("Ribbon", module).add("base", () => (
+  <Theme color={orange}>
+    <div
+      style={{
+        backgroundColor: "blue",
+        width: 200,
+        height: 200,
+        position: "relative",
+      }}
+    >
+      <Ribbon>Ribbon Text</Ribbon>
+    </div>
+  </Theme>
+));
--- src/atoms/typography/typography.stories.tsx
+++ src/atoms/typography/typography.stories.tsx
@@ -2,27 +2,26 @@ import { storiesOf } from "@storybook/react";
 import React from "react";
 
 import { orange } from "../../theme/colors/colors";
-import { Theme } from "../../theme/theme/theme";
+import { Theme } from "../../theme/theme-context/theme-context";
 
 import { Typography, Variant } from "./typography";
 
-storiesOf("Typography", module)
-  .add("variants", () => (
-    <Theme color={orange}>
-      {[
-        'body-1',
-        'body-2',
-        'button',
-        'detail',
-        'form-label',
-        'heading-1',
-        'heading-2',
-        'status',
-        'title',
-      ].map((variant: Variant) => (
-        <div style={{ marginBottom: 16 }}>
-          <Typography variant={variant}>This is some text!</Typography>
-        </div>
-      ))}
-    </Theme>
-  ));
+storiesOf("Typography", module).add("variants", () => (
+  <Theme color={orange}>
+    {[
+      "body-1",
+      "body-2",
+      "button",
+      "detail",
+      "form-label",
+      "heading-1",
+      "heading-2",
+      "status",
+      "title",
+    ].map((variant: Variant) => (
+      <div style={{ marginBottom: 16 }}>
+        <Typography variant={variant}>This is some text!</Typography>
+      </div>
+    ))}
+  </Theme>
+));
