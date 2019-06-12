433eebcbdb938b1b8ddb56868f9652b746f626fc
Leo Bernard <admin@leolabs.org>
2019-06-12 12:14:36 +0200

    :sparkles: Add Theme component

---
+++ src/theme/theme/theme.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Color } from "theme/colors/colors";
+import { ColorCombination } from "theme/color-combinations/color-combinations";
+
+interface ThemeProps {
+  color?: Color;
+  colorCombination?: ColorCombination;
+}
+
+const createVariables = (color: Color) =>
+  Object.keys(color).reduce(
+    (acc, cur) => {
+      acc[`--color-${cur}`] = color[cur];
+      return acc;
+    },
+    {} as { [k: string]: string },
+  );
+
+export const Theme: React.FC<ThemeProps> = ({ color, colorCombination, children }) => {
+  const style = {
+    ...(color ? createVariables(color) : {}),
+    ...(colorCombination
+      ? { "--color-fg": colorCombination.fg, "--color-bg": colorCombination.bg }
+      : {}),
+  };
+
+  return <div style={style}>{children}</div>;
+};
