89e78a1b03ea4405f9a7f19e8415f380f84190a6
Leo Bernard <admin@leolabs.org>
2019-06-13 21:37:38 +0200

    :zap: Don't show all available icons but only a subset to reduce file sizes

--- src/atoms/icons/icons.stories.tsx
+++ src/atoms/icons/icons.stories.tsx
@@ -1,21 +1,44 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import { FeatherIcon, iconCl, FontAwesomeIcon } from "./icons";
-import * as featherIcons from "react-feather";
-import * as fontAwesomeIcons from "@fortawesome/free-solid-svg-icons";
+import { Icon as Feather, Menu, ChevronRight, Clock, Info, User } from "react-feather";
+import {
+  faSun,
+  faGlassCheers,
+  faMusic,
+  faBirthdayCake,
+  faBicycle,
+  IconDefinition,
+} from "@fortawesome/free-solid-svg-icons";
 import { styled } from "linaria/react";
 
+const featherIcons: { [k: string]: Feather } = {
+  Menu,
+  ChevronRight,
+  Clock,
+  Info,
+  User,
+};
+
+const faIcons: { [k: string]: IconDefinition } = {
+  faSun,
+  faGlassCheers,
+  faMusic,
+  faBirthdayCake,
+  faBicycle,
+};
+
 const Icons = styled.div`
-  display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(128px, 1fr));
-  grid-gap: 1rem;
+  display: flex;
+  flex-wrap: wrap;
+
+  margin-bottom: 2rem;
 
   .${iconCl} {
     display: block;
-    width: 100%;
+    width: 4rem;
     height: auto;
     padding: 32px;
-    box-sizing: border-box;
   }
 `;
 
@@ -26,24 +49,32 @@ const Icon = styled.div`
 
 storiesOf("Atoms / Icons", module)
   .add("Feather Icons", () => (
-    <Icons>
-      {Object.entries(featherIcons).map(([name, icon]) => (
-        <Icon>
-          <FeatherIcon icon={icon} />
-          <span>{name}</span>
-        </Icon>
-      ))}
-    </Icons>
+    <>
+      <Icons>
+        {Object.entries(featherIcons).map(([name, icon]) => (
+          <Icon>
+            <FeatherIcon icon={icon} />
+            <span>{name}</span>
+          </Icon>
+        ))}
+      </Icons>
+      <a href="https://feathericons.com/" target="_blank">
+        More Feather Icons
+      </a>
+    </>
   ))
   .add("FontAwesome", () => (
-    <Icons>
-      {Object.entries(fontAwesomeIcons)
-        .filter(([name, icon]) => (icon as any).icon)
-        .map(([name, icon]) => (
+    <>
+      <Icons>
+        {Object.entries(faIcons).map(([name, icon]) => (
           <Icon>
             <FontAwesomeIcon icon={icon as any} />
             <span>{name}</span>
           </Icon>
         ))}
-    </Icons>
+      </Icons>
+      <a href="https://fontawesome.com/icons" target="_blank">
+        More FontAwesome Icons
+      </a>
+    </>
   ));
