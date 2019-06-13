c338ede13c4c85f88be6b39f5e401fc0aa2b3ab3
Marcus Weiner <marcus.weiner@gmail.com>
2019-06-13 11:10:53 +0200

    🎨 Use storybook action for reporting date

--- src/atoms/datetime-picker/datetime-picker.stories.tsx
+++ src/atoms/datetime-picker/datetime-picker.stories.tsx
@@ -1,13 +1,14 @@
 import React, { useState, useCallback } from "react";
 import { storiesOf } from "@storybook/react";
 import { DatetimePicker } from "./datetime-picker";
+import { action } from "@storybook/addon-actions";
 
 const ControlledDateTimePicker = () => {
   const [date, setDate] = useState(new Date(2019, 5, 2, 13, 37));
   const handleDateChange = useCallback(
     ({ value }) => {
-      console.log("datetime changed:", value);
       setDate(value);
+      action("datetime-changed")({ value });
     },
     [setDate],
   );
