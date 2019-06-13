cb8011291a3623fb8057f06dbad596498aa76db2
Marcus Weiner <marcus.weiner@gmail.com>
2019-06-13 15:33:50 +0200

    💄 Fix sizing properties of ResponsiveAspect helper

--- src/layout/responsive-aspect/responsive-aspect.tsx
+++ src/layout/responsive-aspect/responsive-aspect.tsx
@@ -1,16 +1,26 @@
 import React from "react";
 import { styled } from "linaria/react";
 
-const ResponsiveAspectInner = styled.div<{ ratio: number }>`
-  position: relative;
-  height: 0;
-  padding-bottom: ${p => p.ratio * 100}%;
+const ResponsiveAspectInner = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
 `;
 
-const ResponsiveAspectOuter = styled.div<{ ratio: number }>``;
+const ResponsiveAspectOuter = styled.div<{ ratio: number }>`
+  position: relative;
+  display: flex;
+
+  &:before {
+    content: "";
+    padding-bottom: ${p => p.ratio * 100}%;
+  }
+`;
 
 export const ResponsiveAspect: typeof ResponsiveAspectOuter = props => (
   <ResponsiveAspectOuter {...props}>
-    <ResponsiveAspectInner ratio={props.ratio}>{props.children}</ResponsiveAspectInner>
+    <ResponsiveAspectInner>{props.children}</ResponsiveAspectInner>
   </ResponsiveAspectOuter>
 );
