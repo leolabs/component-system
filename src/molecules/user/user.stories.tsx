f5d2320becacd6ab268dc1b0077cb69a58619040
Leo Bernard <admin@leolabs.org>
2019-06-13 20:52:51 +0200

    :art: Improve structure of multiple User story

--- src/molecules/user/user.stories.tsx
+++ src/molecules/user/user.stories.tsx
@@ -4,6 +4,26 @@ import { User, UserData } from "./user";
 import { List } from "../../layout/list/list";
 import { action } from "@storybook/addon-actions";
 
+const users: UserData[] = [
+  {
+    id: "mp",
+    name: "Martha Johnson",
+    status: "declined",
+  },
+  {
+    id: "al",
+    name: "Ada Lovelace",
+    status: "accepted",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c0/Ada_Lovelace_Chalon_portrait.jpg/240px-Ada_Lovelace_Chalon_portrait.jpg",
+  },
+  {
+    id: "jd",
+    name: "John Doe",
+    status: "not-sent",
+  },
+];
+
 storiesOf("Molecules / User", module)
   .add("default", () => (
     <User
@@ -16,32 +36,14 @@ storiesOf("Molecules / User", module)
       onShare={action("share")}
     />
   ))
-  .add("multiple", () =>
-    ([
-      {
-        id: "mp",
-        name: "Martha Johnson",
-        status: "declined",
-      },
-      {
-        id: "al",
-        name: "Ada Lovelace",
-        status: "accepted",
-        image:
-          "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c0/Ada_Lovelace_Chalon_portrait.jpg/240px-Ada_Lovelace_Chalon_portrait.jpg",
-      },
-      {
-        id: "jd",
-        name: "John Doe",
-        status: "not-sent",
-      },
-    ] as UserData[]).map((user: UserData) => (
-      <List>
+  .add("multiple", () => (
+    <List>
+      {users.map((user: UserData) => (
         <User
           data={user}
           onDelete={action("delete").bind(null, user)}
           onShare={action("share").bind(null, user)}
         />
-      </List>
-    )),
-  );
+      ))}
+    </List>
+  ));
