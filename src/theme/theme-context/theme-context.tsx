2a48e8ac34cde82fc68b9b5511e7879fcfd3b8b1
Leo Bernard <admin@leolabs.org>
2019-06-13 09:51:43 +0200

    :sparkles: Set font family in theme instead of typography

--- src/theme/theme-context/theme-context.tsx
+++ src/theme/theme-context/theme-context.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import { Color, primary } from "../colors/colors";
+import { css } from "linaria";
 
 interface ThemeProps {
   color?: Color;
 }
+
+const themeCss = css`
+  font-family: '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"';
+`;
 
 const createVariables = (color: Color) =>
   Object.keys(color).reduce(
@@ -16,5 +21,9 @@ const createVariables = (color: Color) =>
 
 export const Theme: React.FC<ThemeProps> = ({ color, children }) => {
   const style = color ? createVariables(color) : createVariables(primary);
-  return <div style={style}>{children}</div>;
+  return (
+    <div style={style} className={themeCss}>
+      {children}
+    </div>
+  );
 };
