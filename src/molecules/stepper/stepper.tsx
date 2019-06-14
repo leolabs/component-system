6a3194f7d6cee4f145a1e2ac4230e0da649c9943
Leo Bernard <admin@leolabs.org>
2019-06-14 16:19:04 +0200

    :pencil: Document modules/Stepper

--- src/molecules/stepper/stepper.tsx
+++ src/molecules/stepper/stepper.tsx
@@ -22,13 +22,40 @@ const StyledButton = styled(Button)`
   margin-left: 1rem;
 `;
 
+/**
+ * Properties for the Stepper component.
+ */
 interface StepperProps {
+  /**
+   * The current step.
+   */
   step: number;
+
+  /**
+   * The maximum number of steps.
+   */
   maxStep: number;
+
+  /**
+   * Gets called whenever the user navigates forward or backward one step.
+   */
   onChange?: (step: number) => any;
+
+  /**
+   * Gets called when the user clicks on the finish button.
+   */
   onFinish?: () => any;
 }
 
+/**
+ * A stepper with status and back/forward buttons.
+ *
+ * The buttons don't alter the state themselves, so you'll have to do that in your
+ * component. An example for that is available in Storybook.
+ *
+ * @example
+ * <Stepper step={2} maxStep={3} onChange={console.log} onFinish={console.log} />
+ */
 export const Stepper: React.FC<StepperProps & Omit<React.ComponentProps<"div">, "onChange">> = ({
   step,
   maxStep,
