9ffefe4a075f43242d9f0ca1d32d75623934ea9c
Moritz Gunz <moritz.gunz@gmail.com>
2019-06-14 15:44:59 +0200

    :pencil2: Docs for input
    Closes #15

--- src/atoms/input/input.tsx
+++ src/atoms/input/input.tsx
@@ -5,8 +5,19 @@ import { styled } from "linaria/react";
 import { neutral, orange } from "../../theme/colors/colors";
 import { shortTime } from "../../theme/timing/timing";
 
+/** Props for the input component. */
 export interface InputProps extends ComponentProps<"input"> {
+  /**
+   * Whether the input should use a multiline input component (i. e. textarea)
+   * or not.
+   */
   multiline?: boolean;
+
+  /**
+   * When using a multiline input, the number of rows.
+   *
+   * Defaults to 7, must be > 0.
+   */
   rows?: number;
 }
 
@@ -48,6 +59,11 @@ const multiline = css`
   resize: none;
 `;
 
+/**
+ * An input component. Optionally supports multiline input via textarea.
+ *
+ * All props are spread to the respective input / textarea element.
+ */
 export const Input: React.SFC<InputProps> = props => {
   if (typeof props.rows === "number" && props.rows <= 0) {
     throw new Error("rows must be > 0");
