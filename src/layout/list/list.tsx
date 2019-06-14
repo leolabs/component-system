4fa1a13a7dbe7f150ab36867e870eb6ebc992401
Moritz Gunz <moritz.gunz@gmail.com>
2019-06-14 15:52:37 +0200

    :pencil2: Docs for list
    Closes #21

--- src/layout/list/list.tsx
+++ src/layout/list/list.tsx
@@ -1,5 +1,18 @@
 import { styled } from "linaria/react";
 
+/**
+ * An unordered list that uses flex layout.
+ *
+ * Does not show HTML list indicators. Use together with `ListItem`.
+ *
+ * @example
+ * ```
+ * <List>
+ *   <ListItem>Number one</ListItem>
+ *   <ListItem>Number two</ListItem>
+ * </List>
+ * ```
+ */
 export const List = styled.ul`
   display: flex;
   flex-direction: column;
@@ -9,6 +22,19 @@ export const List = styled.ul`
   padding: 0;
 `;
 
+/**
+ * A list item to be used with `List`.
+ *
+ * Enforces a fixed width of `3rem`.
+ *
+ * @example
+ * ```
+ * <List>
+ *   <ListItem>Number one</ListItem>
+ *   <ListItem>Number two</ListItem>
+ * </List>
+ * ```
+ */
 export const ListItem = styled.li`
   display: flex;
   align-items: center;
