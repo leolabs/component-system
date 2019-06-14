3aa2ca39ee0a68bd8182e250865539e6a00f5f0b
Moritz Gunz <moritz.gunz@gmail.com>
2019-06-14 16:05:20 +0200

    :fire: Remove superfluous inner element

--- src/layout/responsive-aspect/responsive-aspect.tsx
+++ src/layout/responsive-aspect/responsive-aspect.tsx
@@ -1,14 +1,6 @@
 import React from "react";
 import { styled } from "linaria/react";
 
-const ResponsiveAspectInner = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-`;
-
 const ResponsiveAspectOuter = styled.div<{ ratio: number }>`
   position: relative;
   display: flex;
@@ -31,9 +23,5 @@ export const ResponsiveAspect: typeof ResponsiveAspectOuter = props => {
     throw new Error("invalid aspect ratio");
   }
 
-  return (
-    <ResponsiveAspectOuter {...props}>
-      <ResponsiveAspectInner>{props.children}</ResponsiveAspectInner>
-    </ResponsiveAspectOuter>
-  );
+  return <ResponsiveAspectOuter {...props}>{props.children}</ResponsiveAspectOuter>;
 };
