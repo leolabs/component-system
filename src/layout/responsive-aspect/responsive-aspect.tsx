670522c524a4019f079f37250ef75a12238b7438
Moritz Gunz <moritz.gunz@gmail.com>
2019-06-14 16:02:44 +0200

    :pencil2: Docs for ResponsiveAspect
    Closes #23

--- src/layout/responsive-aspect/responsive-aspect.tsx
+++ src/layout/responsive-aspect/responsive-aspect.tsx
@@ -19,6 +19,13 @@ const ResponsiveAspectOuter = styled.div<{ ratio: number }>`
   }
 `;
 
+/**
+ * A wrapper component that enforces a fixed aspect `ratio` while not fixing
+ * specific sizes.
+ *
+ * The inner element must be `position: absolute` and
+ * `top / left / bottom / right: 0` (i. e. it needs to fill its parent completely).
+ */
 export const ResponsiveAspect: typeof ResponsiveAspectOuter = props => {
   if (props.ratio <= 0) {
     throw new Error("invalid aspect ratio");
